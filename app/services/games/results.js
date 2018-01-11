1a6aabd88fc900dcfc98a7ed6a9b4dfbddec962e
Mateusz Odelga <mateusz.odelga@schibsted.pl>
2018-01-11 23:44:43 +0100

    Support played flag in results to fix edit invalid play time

--- app/services/games/results.js
+++ app/services/games/results.js
@@ -4,25 +4,35 @@ const rules = require('./rules');
 const table = require('./table');
 const detailedGame = require('./detailedGame');
 
-const getCurrentRound = R.curry((aTable, uid) => R.pipe(R.find(R.propEq('id', uid)), R.prop('played'), R.inc)(aTable));
-const parseResultAsNumbers = ({ home, visitor }) => ({ home: parseInt(home, 10), visitor: parseInt(visitor, 10) });
+const getCurrentRound = R.curry((aTable, uid) =>
+  R.pipe(R.find(R.propEq('id', uid)), R.prop('played'), R.inc)(aTable));
+
+const parseResultAsNumbers = ({ home, visitor }) => ({
+  home: parseInt(home, 10),
+  visitor: parseInt(visitor, 10),
+});
 
 module.exports = {
   async postResult({
     id, gid, result, status,
   }) {
     await rules.postOrUpdate({
-      id, gid, result, status, force: false,
+      id,
+      gid,
+      result,
+      status,
+      force: false,
     });
     const theTable = await table.getTable({ gid });
     const contest = await contests.findById({ id });
     const isRematch = R.has('isRematch', contest)
       ? contest.isRematch
       : (await contests.findById({ id: contest.rematch })).status !== 'SCHEDULED';
-    const [homeRound, visitorRound] = R.map(
-      getCurrentRound(theTable),
-      [contest.home, contest.visitor],
-    );
+    // Will corrupt the data if someone reject match and play another ;(
+    const [homeRound, visitorRound] = R.map(getCurrentRound(theTable), [
+      contest.home,
+      contest.visitor,
+    ]);
     const round = contest.round || {
       home: homeRound,
       visitor: visitorRound,
@@ -34,6 +44,7 @@ module.exports = {
       edited: R.inc(contest.edited || -1),
       round,
       isRematch,
+      played: new Date().toString(),
     });
     await table.updateTable({ gid });
     return detailedGame({ gid });
@@ -43,7 +54,11 @@ module.exports = {
     id, gid, result, status, force, ...other
   }) {
     await rules.postOrUpdate({
-      id, gid, result, status, force,
+      id,
+      gid,
+      result,
+      status,
+      force,
     });
     const contest = await contests.findById({ id });
     await contests.update({
@@ -59,8 +74,18 @@ module.exports = {
   async rejectResult({ id, gid }) {
     await rules.rejectResult({ id, gid });
     const contest = R.pickAll(
-      ['id', 'home', 'visitor', 'gid', 'rematch',
-        'scheduled', 'round', 'isRematch', 'edited'],
+      [
+        'id',
+        'home',
+        'visitor',
+        'gid',
+        'rematch',
+        'scheduled',
+        'round',
+        'isRematch',
+        'edited',
+        'played',
+      ],
       await contests.findById({ id }),
     );
     await contests.update({ ...contest, edited: contest.edited + 1, status: 'SCHEDULED' });
